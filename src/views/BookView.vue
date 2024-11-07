8b8f840802e842e37dac4b3626b626d9e15952e0
Elen Liivapuu <elen.liivapuu@ut.ee>
2024-11-07 16:21:16 +0200

    Implement calendar in BookView view. Fix calendar logic and styling.

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -3,10 +3,10 @@
         <h2 class="page-title">Broneeri</h2>
         
         <section class="two-grid container">
-            <div>
-                <h3>Vali aeg</h3>
+            <div class="calendar-parent">
+                <h3>Vali päev:</h3>
 
-                <div id="calendar">
+                <div id="calendar" class="calendar">
                     <CalendarView
                         :show-date="showDate"
                         :startingDayOfWeek = 1
@@ -18,59 +18,71 @@
                                 <nav class="cv-header-nav">
                                     <button class="previousPeriod" @click="setShowDate(headerProps.previousPeriod)">&lt;</button>
                                     <button class="nextPeriod" @click="setShowDate(headerProps.nextPeriod)">&gt;</button>
-                                    <button class="currentPeriod" @click="setShowDate(headerProps.currentPeriod)">Täna</button>
+                                    <button class="currentPeriod" @click="setTodayAsSelected">Täna</button>
                                 </nav>
                                 <p class="periodLabel" style="margin: 0; padding: 0; min-height: 0;">{{ headerProps.monthNames[headerProps.periodStart.getMonth()] }}</p>
                             </div>
                        </template>
+                       <!-- <template #day="{ day }">
+                            <div 
+                                class="cv-day"
+                                :class="{ 'clicked-day': isSelectedDay(day) }"
+                                @click="onDayClick(day)">
+                                {{ day.getDate() }}
+                            </div>
+                        </template> -->
                     </CalendarView>
                 </div>
             </div>
 
             <div>
-                <h3>Aja broneerimine</h3>
+                <h3>Vali kellaaeg:</h3>
 
                 <div v-if="showSidebar" class="sidebar">
-                <div class="sidebar-content">
-                    <h4>{{ selectedDate.toLocaleDateString() }}</h4>
+                    <div class="sidebar-content">
+                        <!-- <h4>{{ formattedSelectedDate }}</h4> -->
+                        <!--<h4>{{ selectedDate?.toLocaleDateString() || 'Select a date' }}</h4> -->
+                        <h4>{{ formattedSelectedDate }}</h4>
+                        <!-- <h4>{{ selectedDate ? selectedDate.toLocaleDateString() : 'Select a date' }}</h4> -->
 
-                    <select id="timeSlot" v-model="selectedTime" @change="emitSelectedTime" v-if="bookedHours.length>0">
-                        <option value="" disabled>Kellaaeg</option>
+                        <select id="timeSlot" v-model="selectedTime" @change="emitSelectedTime" v-if="bookedHours.length>0">
+                            <option value="" disabled>Kellaaeg</option>
 
-                        <option v-for="time in bookedHours" :key="time" :value="time">
-                            {{ time }}:00
-                        </option>
-                        
-                    </select>
-                    <div v-else>
-                        <p>Vabad ajad puuduvad selleks kuupaevaks!</p>
+                            <option v-for="time in bookedHours" :key="time" :value="time">
+                                {{ time }}:00
+                            </option>
+                            
+                        </select>
+                        <div v-else>
+                            <p>Vabad ajad puuduvad selleks kuupaevaks!</p>
+                        </div>
+
+                        <br><br>
                     </div>
-
-                    <br><br>
-                </div>
                 </div>
                 
                 <div v-if="successMsg" class="alert success">{{ successMsg }}</div>
                 <div v-if="errorMsg" class="alert error">{{ errorMsg }}</div>
+                
+            </div>
+        </section>
+        <section class="contact-container container">
+            <div class="contact-form">
+                <h3>Sisesta andmed:</h3>
                 <form @submit.prevent="submitForm" method="post" class="contact-form">
                     <label for="name">Nimi:</label>   
                     <input type="text" id="name" v-model="formData.name" required>
-                    <br>
 
                     <label for="phone">Telefon:</label>  
                     <input type="text" id="phone" v-model="formData.phone" required>
-                    <br>
+
                     <label for="email">E-post:</label>    
                     <input type="email" id="email" v-model="formData.email" required>
-
-                    <br><br>
+                    <br>
                     <button type="submit" id="confirm" :disabled="!isFormValid">Broneeri</button>
                 </form>
             </div>
         </section>
-
-        
-
     </main>
 </template>
 
@@ -87,6 +99,7 @@
 			return { 
                 showDate: new Date(),
                 selectedDate: null,
+                previousSelectedElement: null,
                 showSidebar: false,
                 bookedHours: [],
                 selectedTime: "",
@@ -103,7 +116,19 @@
         computed: {
             isFormValid() {
                 return this.formData.name && this.formData.phone && this.formData.email && this.selectedTime;
-            }
+            },
+             isSelectedDay(day) {
+                // Check if the provided day is the selected date
+                return this.selectedDate && this.selectedDate.toDateString() === day.toDateString();
+            },
+            //only for showing date in the broneeri sidebar in dd.mm.yyyy format
+            formattedSelectedDate() { 
+                if (!this.selectedDate) return '';
+                const day = String(this.selectedDate.getDate()).padStart(2, '0');
+                const month = String(this.selectedDate.getMonth() + 1).padStart(2, '0');
+                const year = this.selectedDate.getFullYear();
+                return `${day}.${month}.${year}`;
+    }
         },
 		components: {
 			CalendarView,
@@ -111,15 +136,45 @@
 		},
 		methods: {
 			setShowDate(d) {
-				this.showDate = d;
+                this.showDate = d;
+                
+                if (d.toDateString() === new Date().toDateString()) {
+                    this.setTodayAsSelected();
+                }
             },
-            onDayClick(date) {
+            setTodayAsSelected() {
+                // Clear previous selection
+                if (this.previousSelectedElement) {
+                    this.previousSelectedElement.classList.remove("clicked-day");
+                    this.previousSelectedElement = null;
+                }    
+                    // Set today as the selected date
+                this.selectedDate = new Date();
+                this.setShowDate(this.selectedDate);
+                this.showSidebar = true;
+            },
+            onDayClick(date, calendarItems, windowEvent) {
+                this.selectedTime = "";
+                // Remove `clicked-day` class from previously selected element
+                if (this.previousSelectedElement) {
+                    this.previousSelectedElement.parentNode.classList.remove("clicked-day");
+                    this.previousSelectedElement.classList.remove("clicked-day");
+                }
+                console.log("Clicked date:", date);
                 this.selectedDate = date;
                 this.showSidebar = true;
 
+                var dayElement = windowEvent.srcElement;
+                if (dayElement.classList.contains("cv-day")) {
+                    dayElement.classList.add("clicked-day");
+                } else if (dayElement.classList.contains("cv-day-number")) {
+                    dayElement.parentNode.classList.add("clicked-day");
+                }
+                this.previousSelectedElement = windowEvent.srcElement;
+
                 // Example list of available dates
                 this.bookedHours = [8, 10, 12, 14, 16];
-                
+
                 // Get bookings for the selected date
                 let dayBookings = this.events
                     .filter(event => new Date(event.startDate).toDateString() === date.toDateString())
@@ -127,9 +182,11 @@
 
                 this.bookedHours = this.bookedHours.filter(h => !dayBookings.includes(h));
             },
+            //onItemClick(date) {},
             emitSelectedTime(e) {
                 this.selectedDate.setHours(e.target.value);
             },
+            
             async loadBookings() {
                 try {
                     const response = await fetch('/api/bookings');
@@ -185,10 +242,12 @@
         mounted() {
             this.loadBookings();
         }
-	};
+    };
+    ///////////////////////////////////////////////////////////////////
+    /////////
 </script>
 
-<style scoped>
+<style>
 main {
     background-color: #333;
 }
@@ -196,17 +255,41 @@ main {
 .two-grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    column-gap: 3em;
+    column-gap: 6em;
     row-gap: 1em;
     color: white;
     padding-bottom: 2em;
 }
+.calendar-parent {
+    max-width: 100%;
+    max-height: 100%;
+}
 
-#calendar {
+.calendar {
     display: flex;
     flex-direction: column;
     background-color: #fff;
+    height: 400px;
+    border-radius: 4px;
 }
+
+.cv-wrapper {
+    border-radius: 4px;
+}
+
+
+.periodLabel {
+    display: grid;
+    margin: 10px; 
+    padding: 4px; 
+    min-height: 0;
+    /* locale: et; */
+    
+}
+
+/* #calendar cv-wrapper locale-et {
+    
+} */
 
 /* Sidebar styling */
 .sidebar {
@@ -227,19 +310,6 @@ main {
     padding: 0.5em 0;
 }
 
-.sidebar-content button {
-    background-color: #f39c12;
-    color: #fff;
-    border: none;
-    padding: 0.5em 1em;
-    cursor: pointer;
-    transition: background-color 0.2s ease;
-}
-
-.sidebar-content button:hover {
-    background-color: #e67e22;
-}
-
 select {
   padding: 0.5em;
   font-size: 1em;
@@ -254,6 +324,7 @@ select:focus {
   outline: none;
   box-shadow: 0 0 5px rgba(255, 221, 0, 0.5);
 }
+
 .alert {
     padding: 0.5em;
     margin-bottom: 1em;
@@ -272,4 +343,104 @@ select:focus {
     color: #721c24;
     border: 1px solid #f5c6cb;
 }
+.theme-gcal .cv-day-number {
+    margin: 0 auto; /*0 auto */
+    padding: 0.5em 0.5em
+}
+.theme-gcal .cv-day.today .cv-day-number{
+    background-color: gray;
+}
+#calendar .previousPeriod:hover, #calendar .nextPeriod:hover{
+    cursor: pointer;
+    background-color: #f0f0f0;
+    border-radius: 4px;
+}
+#calendar .currentPeriod:hover {
+    cursor: pointer;
+    background-color: #f0f0f0;
+}
+#calendar .currentPeriod:active {
+    background-color: gray;
+    transition: background-color 0.5s ease;
+}
+#calendar .cv-day {
+    cursor: pointer; /* Show pointer cursor on hover */
+    padding: 0.5em;
+    transition: background-color 0.3s ease;
+}
+
+#calendar .cv-day-number {
+    background-color: none;
+}
+
+#calendar .cv-day:hover {
+    background-color: #f0f0f0; /* Light gray background on hover */
+}
+
+#calendar .clicked-day {
+    background-color: #ffdd00 !important; 
+    color: #333 !important; 
+}
+
+.contact-container {
+    display: flex;
+    column-gap: 0.5em;
+    row-gap: 0.7em;
+    margin-top: 1em;
+    
+}
+
+.contact-form {
+    width: 100%;
+    max-width: 600px;
+
+}
+
+.contact-form form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    margin: 0;
+    
+}
+.contact-form h3 {
+    color: #fff;
+}
+
+.contact-form label {
+    text-align: left;
+    margin-bottom: 3px;
+    color: white;
+    font-weight: bold;
+}
+
+.contact-form input,
+.contact-form textarea {
+    padding: 10px;
+    margin-bottom: 8px;
+    border: none;
+    border-radius: 4px;
+    font-size: 0.9em;
+    color: #000;
+}
+.contact-form button {
+    padding: 15px;
+    margin-top: 0.7em;
+    margin-bottom: 0.7em;
+    background-color: yellow;
+    color: #000;
+    border: none;
+    border-radius: 4px;
+    font-size: 1.2em;
+    cursor: pointer;
+    font-weight: bold;
+
+}
+
+.contact-form button:hover {
+    background-color: #000;
+    color: #fff;
+}
+
 </style>
